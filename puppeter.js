473dff2018e80a9abfff499507daba61d5797d4c
Peter Ramos <dchamp16@gmail.com>
2023-12-21 21:33:43 -0700

    updated the puppeter

--- puppeter.js
+++ puppeter.js
@@ -1,22 +1,50 @@
 import puppeteer from 'puppeteer';
 
 (async () => {
-  // Launch the browser and open a new blank page
-  const browser = await puppeteer.launch();
+  // Launches a new browser instance in the new headless mode
+  const browser = await puppeteer.launch({
+    headless: "new"
+  });
+
+  // Opens a new page (tab) in the browser
   const page = await browser.newPage();
 
-  // Navigate the page to a URL
+  // Navigates to the specified URL (Google's homepage in this case)
   await page.goto('https://www.google.com/');
 
-  // Set screen size
-  await page.setViewport({width: 1080, height: 1024});
+  // Sets the viewport size of the page, simulating a window size
+  await page.setViewport({ width: 1080, height: 1024 });
 
-  // Query for an element handle.
-  const element = await page.$eval('.MV3Tnb',  el => el.textContent)
+  // Selector for Google's search box, waits for the element to be present
+  const searchSelector = 'textarea[name="q"]';
+  await page.waitForSelector(searchSelector);
 
-console.log(element)
+  // Types the search query 'javascript' into the search box
+  await page.type(searchSelector, 'javascript');
 
+  // Simulates pressing the Enter key to submit the search
+  await page.keyboard.press('Enter');
 
-  // Close browser.
+  // Waits for the page navigation to complete, which in this case is the loading of search results
+  await page.waitForNavigation();
+
+  // Selector for the first search result heading, waits for the element to be present
+  const resultSelector = 'h3.LC20lb';
+  await page.waitForSelector(resultSelector);
+
+  // Clicks on the first search result heading
+  await page.click(resultSelector);
+
+  // Waits for the navigation to complete after clicking on the search result, which is likely to lead to a new page
+  await page.waitForNavigation();
+
+  // Queries for an element with the class 'javascript-logo' and retrieves its 'src' attribute
+  // Ensure that this class exists on the new page and is the correct selector for your use case
+  const element = await page.$eval('.javascript-logo', el => el.src);
+
+  // Logs the 'src' attribute of the found element to the console
+  console.log(element);
+
+  // Closes the browser instance
   await browser.close();
 })();
